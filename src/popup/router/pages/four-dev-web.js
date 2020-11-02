20a67e43d53a7c69d3345994f87fad13ed13b61c
yhq <yueheqing@demogic.com>
2020-11-02 10:35:17 +0800

    fix: 处理链接

--- src/popup/router/pages/four-dev-web.js
+++ src/popup/router/pages/four-dev-web.js
@@ -1,48 +1,47 @@
-
 export const fourDevWeb = [
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '达摩网络',
-              url: 'https://www.demogic.com/'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '运维4.0',
-              url: 'https://four.gicdev.com/operation-platform/#/'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: 'API网关4.0',
-              url: 'https://four.gicdev.com/gateway-web/#/'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '开放平台官网4.0',
-              url: 'https://four.gicdev.com/open-platform/#/index'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '应用中心4.0',
-              url: 'https://four.gicdev.com/app-center/applyIndex'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '服务市场4.0',
-              url: 'https://four.gicdev.com/market/gic/index'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: 'GIC商户后台登录4.0',
-              url: 'https://four.gicdev.com/damo-system/login'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '开发者中心4.0',
-              url: 'https://four.gicdev.com/gic-app-center/'
-            },
-            {
-              src: require('../../../images/logo40.png'),
-              webName: '好办 4.0',
-              url: 'https://four.gicdev.com/haoban-4/#/index'
-            }
-          ]
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '达摩网络',
+    url: 'https://www.demogic.com/',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '运维4.0',
+    url: 'https://four.gicdev.com/operation-platform/#/',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: 'API网关4.0',
+    url: 'https://four.gicdev.com/gateway-web/#/',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '开放平台官网4.0',
+    url: 'https://four.gicdev.com/open-platform/#/index',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '应用中心4.0',
+    url: 'https://four.gicdev.com/app-center/applyIndex',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '服务市场4.0',
+    url: 'https://four.gicdev.com/market/index',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: 'GIC商户后台登录4.0',
+    url: 'https://four.gicdev.com/damo-system/login',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '开发者中心4.0',
+    url: 'https://four.gicdev.com/gic-app-center/',
+  },
+  {
+    src: require('../../../images/logo40.png'),
+    webName: '好办 4.0',
+    url: 'https://four.gicdev.com/haoban-4/contactsList',
+  },
+];
--- src/popup/router/pages/four-pro-web.js
+++ src/popup/router/pages/four-pro-web.js
@@ -1,38 +1,42 @@
-
 export const fourProWeb = [
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '运维4.0',
-              url: 'https://ideal.demogic.com/operation-platform/#/'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: 'API网关4.0',
-              url: 'https://ideal.demogic.com/gateway-web/#/'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '开放平台官网4.0',
-              url: 'https://ideal.demogic.com/open-platform/#/index'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '应用中心4.0',
-              url: 'https://ideal.demogic.com/app-center/applyIndex'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '服务市场4.0',
-              url: 'https://ideal.demogic.com/market/gic/index'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: 'GIC商户后台登录4.0',
-              url: 'https://ideal.demogic.com/damo-system/login'
-            },
-            {
-              src: require('../../../images/logo45.png'),
-              webName: '开发者中心4.0',
-              url: 'https://ideal.demogic.com/gic-app-center/'
-            }
-          ]
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '运维4.0',
+    url: 'https://ideal.demogic.com/operation-platform/#/',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: 'API网关4.0',
+    url: 'https://ideal.demogic.com/gateway-web/#/',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '开放平台官网4.0',
+    url: 'https://ideal.demogic.com/open-platform/#/index',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '应用中心4.0',
+    url: 'https://ideal.demogic.com/app-center/applyIndex',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '服务市场4.0',
+    url: 'https://ideal.demogic.com/market/index',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: 'GIC商户后台登录4.0',
+    url: 'https://ideal.demogic.com/damo-system/login',
+  },
+  {
+    src: require('../../../images/logo45.png'),
+    webName: '开发者中心4.0',
+    url: 'https://ideal.demogic.com/gic-app-center/',
+  },
+  {
+    src: require('../../../images/logo40.png'),
+    webName: '好办 4.0',
+    url: 'https://ideal.demogic.com/haoban-4/contactsList',
+  },
+];
